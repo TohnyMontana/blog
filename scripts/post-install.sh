0057f468875bbd78eb8207162d9ef8418850d956
ThonyMontana <saen420one@gmail.com>
2018-02-09 03:05:14 +0400

    complete

---
+++ scripts/post-install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+yum install -y --nogpgcheck \
+    epel-release \
+    http://rpms.remirepo.net/enterprise/remi-release-7.rpm \
+    https://dev.mysql.com/get/mysql57-community-release-el7-8.noarch.rpm
+
+yum -y update --nogpgcheck
+
+yum install --enablerepo=epel,remi,remi-php72 -y --nogpgcheck \
+    httpd \
+    git \
+    mc \
+    htop \
+    vim \
+    wget \
+    mysql-server \
+    php \
+    php-pecl-apcu \
+    php-common \
+    php-cli \
+    php-pear \
+    php-pdo \
+    php-mysqlnd \
+    php-pecl-memcache \
+    php-pecl-memcached \
+    php-gd \
+    php-mbstring \
+    php-mcrypt \
+    php-xml
+
+systemctl enable httpd.service
+systemctl start httpd.service
+
+systemctl enable mysqld
+systemctl start mysqld
+
+MYSQL_TEMP_PWD=`cat /var/log/mysqld.log | grep 'A temporary password is generated' | awk -F'root@localhost: ' '{print $2}'`
+mysqladmin -u root -p`echo $MYSQL_TEMP_PWD` password 'root'
+
+cat << EOF > .my.cnf
+[client]
+user=root
+password=root
+EOF
+
+curl https://getcomposer.org/composer.phar -o /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
+
+curl https://phar.phpunit.de/phpunit-7.phar -o /usr/local/bin/phpunit
+chmod +x /usr/local/bin/phpunit
