7b5c0339669efcb4caed6d94a40a16400dd8d066
ThonyMontana <saen420one@gmail.com>
2018-02-17 04:31:13 +0400

    database 2.0

--- schema.sql
+++ schema.sql
@@ -1,54 +1,29 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE IF NOT EXISTS `article` (
+  `id`           INT           UNSIGNED NOT NULL AUTO_INCREMENT,
+  `userId`       INT           UNSIGNED NOT NULL,
+  `title`        VARCHAR(255)           NOT NULL,
+  `description`  TEXT                   NULL,
+  `text`         TEXT                   NULL,
+  `state`        TINYINT       UNSIGNED NOT NULL,
+  `createdAt`    TIMESTAMP              NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+)
+  ENGINE=InnoDB 
+  DEFAULT CHARSET=utf8;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
-CREATE TABLE `article` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `text` text NOT NULL,
-  `state` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `login` varchar(50) NOT NULL,
-  `mail` varchar(50) NOT NULL,
-  `token` int(11) NOT NULL,
-  `role` varchar(6) NOT NULL,
-  `name` varchar(25) NOT NULL,
-  `lName` varchar(50) NOT NULL,
-  `patronymic` varchar(50) NOT NULL,
-  `birthday` date NOT NULL,
-  `gender` varchar(7) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `article`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
-
-ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `article`
-  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id`           INT           UNSIGNED NOT NULL AUTO_INCREMENT,
+  `password`     CHAR(60)               NOT NULL,
+  `login`        VARCHAR(50)            NOT NULL,
+  `email`        VARCHAR(50)            NOT NULL,
+  `role`         TINYINT       UNSIGNED NOT NULL,
+  `firstName`    VARCHAR(25)            NULL,
+  `lastName`     VARCHAR(25)            NULL,
+  `birthday`     DATE                   NOT NULL,
+  `state`        TINYINT       UNSIGNED NOT NULL,
+  `gender`       TINYINT       UNSIGNED NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY  `name` (`name`)
+)  
+  ENGINE=InnoDB 
+  DEFAULT CHARSET=utf8;
