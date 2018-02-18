77439ac0d89f6b2b6f4705de252c4e9282b2cdc6
ThonyMontana <saen420one@gmail.com>
2018-02-19 02:20:12 +0400

    database 2.5

--- schema.sql
+++ schema.sql
@@ -1,29 +1,31 @@
 CREATE TABLE IF NOT EXISTS `article` (
   `id`           INT           UNSIGNED NOT NULL AUTO_INCREMENT,
   `userId`       INT           UNSIGNED NOT NULL,
+  `createdAt`    TIMESTAMP              NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `state`        TINYINT       UNSIGNED NOT NULL,
   `title`        VARCHAR(255)           NOT NULL,
   `description`  TEXT                   NULL,
   `text`         TEXT                   NULL,
-  `state`        TINYINT       UNSIGNED NOT NULL,
-  `createdAt`    TIMESTAMP              NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  PRIMARY KEY (`userId`)
 )
   ENGINE=InnoDB 
   DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id`           INT           UNSIGNED NOT NULL AUTO_INCREMENT,
+  `createdAt`    TIMESTAMP              NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `state`        TINYINT       UNSIGNED NOT NULL,
+  `gender`       TINYINT       UNSIGNED NULL,
+  `role`         TINYINT       UNSIGNED NOT NULL,
   `password`     CHAR(60)               NOT NULL,
   `login`        VARCHAR(50)            NOT NULL,
   `email`        VARCHAR(50)            NOT NULL,
-  `role`         TINYINT       UNSIGNED NOT NULL,
   `firstName`    VARCHAR(25)            NULL,
   `lastName`     VARCHAR(25)            NULL,
   `birthday`     DATE                   NOT NULL,
-  `state`        TINYINT       UNSIGNED NOT NULL,
-  `gender`       TINYINT       UNSIGNED NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY  `name` (`name`)
+  UNIQUE KEY `name` (`name`)
 )  
   ENGINE=InnoDB 
   DEFAULT CHARSET=utf8;
